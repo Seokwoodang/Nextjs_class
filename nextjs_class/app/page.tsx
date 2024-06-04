6f8c5ddf2f324681384724074acc0f29bf12ed5e
Seokwoodang <swoo1429@naver.com>
2024-06-04 23:52:42 +0900

    nextjs 91강

--- nextjs_class/app/page.tsx
+++ nextjs_class/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import Header from '../components/header'
 
 export default function Home() {
 
-  console.log('nnn')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header />
       <p><Link href="/about">About Us</Link></p>
     </main>
   );
---
+++ nextjs_class/components/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <>
+            <img src="/logo.png" />
+            <h1>Welcome to this NexJS Course</h1>
+        </>
+    )
+}
